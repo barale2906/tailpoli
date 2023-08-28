7dfd2c19fbf44f795a3cb95a315df458f0d299a2
barale2906 <alexanderbarajas@gmail.com>
2023-08-27 21:49:02 -0500

    Configuración de usuario

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -20,14 +20,17 @@
         <!-- Styles -->
         @livewireStyles
     </head>
-    <body class="font-sans antialiased" x-data="{
+    <body class="font-sans antialiased"
+    :class="{'overflow-hidden':  open}"
+    x-data="{
         open: false,
-    }">
+    }"
+    >
 
         @include('layouts.includes.admin.nav');
         @include('layouts.includes.admin.aside');
 
-        <div class="p-4 sm:ml-64">
+        <div class="p-4 sm:ml-64" x-on:click="open: false">
             <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
                 {{ $slot }}
             </div>
