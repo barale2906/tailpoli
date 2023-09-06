ed0b2440166682a0f352d3858009881fa733cdfe
barale2906 <alexanderbarajas@gmail.com>
2023-09-06 14:38:50 -0500

    Editar registro

--- app/Livewire/Admin/Salud/RegimenSalud.php
+++ app/Livewire/Admin/Salud/RegimenSalud.php
@@ -16,6 +16,7 @@ class RegimenSalud extends Component
     public $id = '';
     public $status = true;
     public $estado = false;
+    public $regimenElegido;
 
 
     protected $listeners = ['refresh' => '$refresh'];
@@ -23,17 +24,17 @@ class RegimenSalud extends Component
     /**
      * Reglas de validación
      */
-    protected $rules = [
+    /* protected $rules = [
         'name' => 'required|max:100'
-    ];
+    ]; */
 
     /**
      * Reset de todos los campos
      * @return void
      */
-    public function resetFields(){
+    /* public function resetFields(){
         $this->reset('name');
-    }
+    } */
 
     //Activar evento
     #[On('created-regimen')]
@@ -42,42 +43,46 @@ class RegimenSalud extends Component
     {
         $this->is_modify = !$this->is_modify;
         $this->is_creating = !$this->is_creating;
-        $this->resetFields();
+        //$this->resetFields();
     }
 
+    //Activar evento
+    #[On('Editando-regimen')]
     //Mostrar formulario de creación
     public function updatedIsEditing()
     {
         $this->is_modify = !$this->is_modify;
-        $this->resetFields();
+        $this->is_editing = !$this->is_editing;
     }
     //Mostrar formulario de inactivación
     public function updatedIsDeleting()
     {
         $this->is_modify = !$this->is_modify;
-        $this->resetFields();
+        //$this->resetFields();
     }
 
     // Mostrar Regimen de Salud
     public function showRegimen($regimen, $act){
+
+        $this->regimenElegido=$regimen;
+
         $this->is_modify = !$this->is_modify;
         if($act===0){
             $this->is_editing=!$this->is_editing;
         }else{
             $this->is_deleting=!$this->is_deleting;
         }
-        $this->name=$regimen['name'];
+        /* $this->name=$regimen['name'];
         $this->id=$regimen['id'];
         if($regimen['status']===1){
             $this->status=true;
         }else{
             $this->status=false;
-        }
-        //dd($regimen['status'],$this->status);
+        } */
     }
 
     //Actualizar Regimen de Salud
-    public function editRegimen()
+    /* public function editRegimen()
     {
         // validate
         $this->validate();
@@ -94,7 +99,7 @@ class RegimenSalud extends Component
         $this->is_editing = false;
         $this->is_modify = !$this->is_modify;
         $this->dispatch('refresh');
-    }
+    } */
 
     //Inactivar Regimen de Salud
     public function inactivarRegimen()
