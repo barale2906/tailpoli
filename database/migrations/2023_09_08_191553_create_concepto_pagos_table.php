a070d40a6d0c9e2b57054f1d6b0a9ed5a399d8ca
barale2906 <alexanderbarajas@gmail.com>
2023-09-08 19:23:48 -0500

    modelo, migra, seede

---
+++ database/migrations/2023_09_08_191553_create_concepto_pagos_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('concepto_pagos', function (Blueprint $table) {
+            $table->comment('Descripción de los conceptos de pago generados');
+            $table->id();
+            $table->string('name')->comment('nombre del concepto de pago configurado');
+            $table->boolean('status')->default(true)->comment('false Inactivo, true activo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('concepto_pagos');
+    }
+};
---
+++ database/seeders/ConceptoPagoSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ConceptoPagoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\Financiera\ConceptoPago::create([
+            'name'              =>'Matricula'
+        ]);
+
+        \App\Models\Financiera\ConceptoPago::create([
+            'name'              =>'Cuota Inicial'
+        ]);
+
+        \App\Models\Financiera\ConceptoPago::create([
+            'name'              =>'Semestre'
+        ]);
+
+        \App\Models\Financiera\ConceptoPago::create([
+            'name'              =>'Mensualidad'
+        ]);
+
+        \App\Models\Financiera\ConceptoPago::create([
+            'name'              =>'Kit Uniforme'
+        ]);
+
+        \App\Models\Financiera\ConceptoPago::create([
+            'name'              =>'Kit Herramienta'
+        ]);
+    }
+}
