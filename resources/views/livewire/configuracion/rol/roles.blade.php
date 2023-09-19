078e71622bc4b6c0ccc5d5607134e2a65190e8ca
barale2906 <alexanderbarajas@gmail.com>
2023-09-19 11:44:51 -0500

    Inactivar rol

--- resources/views/livewire/configuracion/rol/roles.blade.php
+++ resources/views/livewire/configuracion/rol/roles.blade.php
@@ -55,9 +55,11 @@
                             </th>
                             <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                 @can('co_rolEditar')
-                                    <a href="#" wire:click.prevent="show({{$role}},{{0}})" class="text-black bg-gradient-to-r from-blue-300 via-blue-400 to-blue-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 capitalize">
-                                        <i class="fa-solid fa-marker"></i>
-                                    </a>
+                                    @if ($role->status===1)
+                                        <a href="#" wire:click.prevent="show({{$role}},{{0}})" class="text-black bg-gradient-to-r from-blue-300 via-blue-400 to-blue-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 capitalize">
+                                            <i class="fa-solid fa-marker"></i>
+                                        </a>
+                                    @endif
                                 @endcan
                                 @can('co_rolInactivar')
                                     <a href="#" wire:click.prevent="show({{$role}},{{1}})" class="text-black bg-gradient-to-r from-yellow-300 via-yellow-400 to-yellow-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-yellow-200 dark:focus:ring-yellow-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 capitalize">
