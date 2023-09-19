4ad7a8513c9c9f404e57c989f5fdcb8bdd75ea6a
barale2906 <alexanderbarajas@gmail.com>
2023-09-19 11:35:18 -0500

    Crear roles

--- app/Livewire/Configuracion/Rol/RolesCreate.php
+++ app/Livewire/Configuracion/Rol/RolesCreate.php
@@ -3,17 +3,20 @@
 namespace App\Livewire\Configuracion\Rol;
 
 use Livewire\Component;
+use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
 class RolesCreate extends Component
 {
     public $name = '';
+    public $permis = [];
 
     /**
      * Reglas de validación
      */
     protected $rules = [
-        'name' => 'required|max:100'
+        'name' => 'required|max:100',
+        'permis'=>'required'
     ];
 
     /**
@@ -21,7 +24,8 @@ class RolesCreate extends Component
      * @return void
      */
     public function resetFields(){
-        $this->reset('name');
+        $this->reset('name', 'permis');
+
     }
 
     // Crear Regimen de Salud
@@ -36,9 +40,12 @@ class RolesCreate extends Component
             $this->dispatch('alerta', name:'Ya existe este rol: '.$this->name);
         } else {
             //Crear registro
-            Role::create([
+            $rol = Role::create([
                 'name'=>strtolower($this->name),
             ]);
+
+            //Asignar permisos
+            $rol->givePermissionTo($this->permis);
 
             // Notificación
             $this->dispatch('alerta', name:'Se ha creado correctamente el rol: '.$this->name);
@@ -50,8 +57,65 @@ class RolesCreate extends Component
         }
     }
 
+    private function permisosac(){
+        return Permission::where('name', 'like', 'ac%')
+                            ->orderBy('id')
+                            ->get();
+    }
+
+    private function permisosca(){
+        return Permission::where('name', 'like', 'ca%')
+                            ->orderBy('id')
+                            ->get();
+    }
+
+    private function permisosfi(){
+        return Permission::where('name', 'like', 'fi%')
+                            ->orderBy('id')
+                            ->get();
+    }
+
+    private function permisosin(){
+        return Permission::where('name', 'like', 'in%')
+                            ->orderBy('id')
+                            ->get();
+    }
+
+    private function permisosre(){
+        return Permission::where('name', 'like', 're%')
+                            ->orderBy('id')
+                            ->get();
+    }
+
+    private function permisosad(){
+        return Permission::where('name', 'like', 'ad%')
+                            ->orderBy('id')
+                            ->get();
+    }
+
+    private function permisosar(){
+        return Permission::where('name', 'like', 'ar%')
+                            ->orderBy('id')
+                            ->get();
+    }
+
+    private function permisosco(){
+        return Permission::where('name', 'like', 'co%')
+                            ->orderBy('id')
+                            ->get();
+    }
+
     public function render()
     {
-        return view('livewire.configuracion.rol.roles-create');
+        return view('livewire.configuracion.rol.roles-create', [
+            'permisosac'=>$this->permisosac(),
+            'permisosca'=>$this->permisosca(),
+            'permisosfi'=>$this->permisosfi(),
+            'permisosin'=>$this->permisosin(),
+            'permisosre'=>$this->permisosre(),
+            'permisosad'=>$this->permisosad(),
+            'permisosar'=>$this->permisosar(),
+            'permisosco'=>$this->permisosco(),
+        ]);
     }
 }
--- resources/views/livewire/configuracion/rol/roles-create.blade.php
+++ resources/views/livewire/configuracion/rol/roles-create.blade.php
@@ -9,6 +9,160 @@
                 <span class="font-medium">¡IMPORTANTE!</span>  {{ $message }} .
             </div>
         @enderror
+
+
+        <div class="p-4 mb-4 text-sm text-cyan-900 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-cyan-500" role="alert">
+            <span class="font-medium">MODULO ACÁDEMICO</span>
+            <div class="grid grid-cols-6 gap-2">
+                @foreach ($permisosac as $item)
+                    <div class="flex items-center mb-4 capitalize">
+                        <input id="default-checkbox" wire:model="permis" type="checkbox" value="{{$item->id}}" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 " >
+                        <label for="default-checkbox" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                            {{$item->descripcion}}
+                        </label>
+                    </div>
+                @endforeach
+                @error('permis')
+                    <div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+                        <span class="font-medium">¡IMPORTANTE!</span>  {{ $message }} .
+                    </div>
+                @enderror
+            </div>
+        </div>
+
+        <div class="p-4 mb-4 text-sm text-cyan-900 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-cyan-500" role="alert">
+            <span class="font-medium">MODULO CARTERA</span>
+            <div class="grid grid-cols-6 gap-2">
+                @foreach ($permisosca as $item)
+                    <div class="flex items-center mb-4 capitalize">
+                        <input id="default-checkbox" wire:model="permis" type="checkbox" value="{{$item->id}}" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 " >
+                        <label for="default-checkbox" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                            {{$item->descripcion}}
+                        </label>
+                    </div>
+                @endforeach
+                @error('permis')
+                    <div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+                        <span class="font-medium">¡IMPORTANTE!</span>  {{ $message }} .
+                    </div>
+                @enderror
+            </div>
+        </div>
+
+        <div class="p-4 mb-4 text-sm text-cyan-900 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-cyan-500" role="alert">
+            <span class="font-medium">MODULO FINANCIERA</span>
+            <div class="grid grid-cols-6 gap-2">
+                @foreach ($permisosfi as $item)
+                    <div class="flex items-center mb-4 capitalize">
+                        <input id="default-checkbox" wire:model="permis" type="checkbox" value="{{$item->id}}" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 " >
+                        <label for="default-checkbox" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                            {{$item->descripcion}}
+                        </label>
+                    </div>
+                @endforeach
+                @error('permis')
+                    <div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+                        <span class="font-medium">¡IMPORTANTE!</span>  {{ $message }} .
+                    </div>
+                @enderror
+            </div>
+        </div>
+
+        <div class="p-4 mb-4 text-sm text-cyan-900 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-cyan-500" role="alert">
+            <span class="font-medium">MODULO INVENTARIO</span>
+            <div class="grid grid-cols-6 gap-2">
+                @foreach ($permisosin as $item)
+                    <div class="flex items-center mb-4 capitalize">
+                        <input id="default-checkbox" wire:model="permis" type="checkbox" value="{{$item->id}}" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 " >
+                        <label for="default-checkbox" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                            {{$item->descripcion}}
+                        </label>
+                    </div>
+                @endforeach
+                @error('permis')
+                    <div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+                        <span class="font-medium">¡IMPORTANTE!</span>  {{ $message }} .
+                    </div>
+                @enderror
+            </div>
+        </div>
+
+        <div class="p-4 mb-4 text-sm text-cyan-900 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-cyan-500" role="alert">
+            <span class="font-medium">MODULO REPORTES</span>
+            <div class="grid grid-cols-6 gap-2">
+                @foreach ($permisosre as $item)
+                    <div class="flex items-center mb-4 capitalize">
+                        <input id="default-checkbox" wire:model="permis" type="checkbox" value="{{$item->id}}" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 " >
+                        <label for="default-checkbox" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                            {{$item->descripcion}}
+                        </label>
+                    </div>
+                @endforeach
+                @error('permis')
+                    <div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+                        <span class="font-medium">¡IMPORTANTE!</span>  {{ $message }} .
+                    </div>
+                @enderror
+            </div>
+        </div>
+
+        <div class="p-4 mb-4 text-sm text-cyan-900 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-cyan-500" role="alert">
+            <span class="font-medium">MODULO ADMINISTRACIÓN</span>
+            <div class="grid grid-cols-6 gap-2">
+                @foreach ($permisosad as $item)
+                    <div class="flex items-center mb-4 capitalize">
+                        <input id="default-checkbox" wire:model="permis" type="checkbox" value="{{$item->id}}" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 " >
+                        <label for="default-checkbox" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                            {{$item->descripcion}}
+                        </label>
+                    </div>
+                @endforeach
+                @error('permis')
+                    <div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+                        <span class="font-medium">¡IMPORTANTE!</span>  {{ $message }} .
+                    </div>
+                @enderror
+            </div>
+        </div>
+
+        <div class="p-4 mb-4 text-sm text-cyan-900 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-cyan-500" role="alert">
+            <span class="font-medium">MODULO ARCHIVO</span>
+            <div class="grid grid-cols-6 gap-2">
+                @foreach ($permisosar as $item)
+                    <div class="flex items-center mb-4 capitalize">
+                        <input id="default-checkbox" wire:model="permis" type="checkbox" value="{{$item->id}}" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 " >
+                        <label for="default-checkbox" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                            {{$item->descripcion}}
+                        </label>
+                    </div>
+                @endforeach
+                @error('permis')
+                    <div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+                        <span class="font-medium">¡IMPORTANTE!</span>  {{ $message }} .
+                    </div>
+                @enderror
+            </div>
+        </div>
+
+        <div class="p-4 mb-4 text-sm text-cyan-900 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-cyan-500" role="alert">
+            <span class="font-medium">MODULO CONFIGURACIÓN</span>
+            <div class="grid grid-cols-6 gap-2">
+                @foreach ($permisosco as $item)
+                    <div class="flex items-center mb-4 capitalize">
+                        <input id="default-checkbox" wire:model="permis" type="checkbox" value="{{$item->id}}" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 " >
+                        <label for="default-checkbox" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                            {{$item->descripcion}}
+                        </label>
+                    </div>
+                @endforeach
+                @error('permis')
+                    <div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+                        <span class="font-medium">¡IMPORTANTE!</span>  {{ $message }} .
+                    </div>
+                @enderror
+            </div>
+        </div>
+
         <button type="submit"
         class="text-white bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-400 dark:hover:bg-blue-500 dark:focus:ring-blue-400"
         >
