fb418af2e4ffd3ad4fab474f6277fe66d660d9e3
barale2906 <alexanderbarajas@gmail.com>
2023-09-19 20:23:56 -0500

    Ajuste boton inactivar

--- resources/views/livewire/configuracion/user/users-inactivar.blade.php
+++ resources/views/livewire/configuracion/user/users-inactivar.blade.php
@@ -7,4 +7,5 @@
     </a>
     <a href="#" wire:click.prevent="$dispatch('Inactivando')" class="text-black bg-gradient-to-r from-red-300 via-red-400 to-red-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-200 dark:focus:ring-red-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 capitalize">
         <i class="fa-solid fa-rectangle-xmark"></i> cancelar
+    </a>
 </div>
