d58fe15e1a50c121643a7066de56b5a19742fff4
barale2906 <alexanderbarajas@gmail.com>
2023-10-07 19:00:44 -0500

    Editar configuración de pagos

--- app/Livewire/Financiera/ConfiguracionPago/ConfiguracionPagosEditar.php
+++ app/Livewire/Financiera/ConfiguracionPago/ConfiguracionPagosEditar.php
@@ -7,6 +7,7 @@ use App\Models\Academico\Modulo;
 use App\Models\Configuracion\Sede;
 use App\Models\Financiera\ConfiguracionPago;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
 class ConfiguracionPagosEditar extends Component
@@ -22,6 +23,8 @@ class ConfiguracionPagosEditar extends Component
     public $curso_id;
     public $id;
     public $modulos;
+
+    public $moduloDepen=[];
 
     /**
      * Reglas de validación
@@ -67,13 +70,44 @@ class ConfiguracionPagosEditar extends Component
         $this->curso_id=$elegido['curso_id'];
         $this->id=$elegido['id'];
 
+        if(!$elegido['incluye']){
+            $this->cargaDep();
+        }
+
         $this->buscaModulos();
+    }
+
+    //Carga dependencias
+    public function cargaDep(){
+        $registros=DB::table('configpago_modulo')
+                        ->where('config_id', $this->id)
+                        ->get();
+
+        foreach ($registros as $value){
+            $nuevo=[
+                'id'=>$value->modulo_id,
+                'name'=>$value->name
+            ];
+
+            if(in_array($nuevo, $this->moduloDepen)){
+
+            }else{
+                array_push($this->moduloDepen, $nuevo);
+            }
+        }
+
     }
 
     //Recalcular
     public function recalcular(){
         $this->calcuCuota();
         $this->calcula();
+    }
+
+    //Cambiar curso
+    public function updatedCursoId(){
+        $this->reset('moduloDepen');
+        $this->buscaModulos();
     }
 
     //Busca modulos
@@ -86,16 +120,27 @@ class ConfiguracionPagosEditar extends Component
 
     //Activa cuotas
     public function calcuCuota(){
-        if($this->valor_curso>$this->valor_cuota_inicial){
-            $this->saldo=$this->valor_curso-$this->valor_matricula-$this->valor_cuota_inicial;
+        if($this->valor_matricula===''){
+            $this->valor_matricula=0;
         }
 
-        if($this->valor_curso===$this->valor_cuota_inicial){
+        if($this->valor_cuota_inicial===''){
+            $this->valor_cuota_inicial=0;
+        }
+
+        $diferencia=$this->valor_cuota_inicial+$this->valor_matricula;
+
+        if($this->valor_curso>$diferencia){
+            $this->saldo=$this->valor_curso-$diferencia;
+        }
+
+        if($this->valor_curso===$diferencia){
             $this->valor_cuota=0;
             $this->cuotas=0;
         }
-        if($this->valor_curso<$this->valor_cuota_inicial){
-            $this->dispatch('alerta', name:'La cuota inicial debe ser menor al valor del curso.');
+
+        if($this->valor_curso<$diferencia){
+            $this->dispatch('alerta', name:'La cuota inicial/matricula debe ser menor al valor del curso.');
             $this->reset(
                 'cuotas',
                 'valor_cuota',
@@ -111,23 +156,96 @@ class ConfiguracionPagosEditar extends Component
         }
     }
 
+    //Elegir los modulos incluidos
+    public function selModulo($id){
+
+        foreach ($this->modulos as $value) {
+            if($value->id===$id){
+                $nuevo=[
+                    'id'=>$id,
+                    'name'=>$value->name
+                ];
+
+                if(in_array($nuevo, $this->moduloDepen)){
+
+                }else{
+                    array_push($this->moduloDepen, $nuevo);
+                }
+
+            };
+
+        }
+    }
+
+    // Eliminar modulo elegido
+    public function elimModulo($id){
+        foreach ($this->modulos as $value) {
+            if($value->id===$id){
+                $nuevo=[
+                    'id'=>$id,
+                    'name'=>$value->name
+                ];
+            }
+        }
+        $indice=array_search($nuevo,$this->moduloDepen,true);
+        unset($this->moduloDepen[$indice]);
+    }
+
     //Actualizar Regimen de Salud
     public function edit()
     {
         // validate
         $this->validate();
 
-        //Actualizar registros
-        ConfiguracionPago::whereId($this->id)->update([
-            'valor_curso'=>$this->valor_curso,
-            'valor_matricula'=>$this->valor_matricula,
-            'valor_cuota_inicial'=>$this->valor_cuota_inicial,
-            'cuotas'=>$this->cuotas,
-            'valor_cuota'=>$this->valor_cuota,
-            'descripcion'=>$this->descripcion,
-            'sede_id'=>$this->sede_id,
-            'curso_id'=>$this->curso_id
-        ]);
+        //eliminar dependencias actuales
+        DB::table('configpago_modulo')
+            ->where('config_id', $this->id)
+            ->delete();
+
+        if($this->valor_matricula===0 && $this->valor_cuota_inicial){
+            $this->valor_matricula=$this->valor_curso;
+        }
+
+        // Verifica inclusión
+        if(count($this->moduloDepen)>0){
+
+            //Editar registro
+            ConfiguracionPago::whereId($this->id)->update([
+                'valor_curso'=>$this->valor_curso,
+                'valor_matricula'=>$this->valor_matricula,
+                'valor_cuota_inicial'=>$this->valor_cuota_inicial,
+                'cuotas'=>$this->cuotas,
+                'valor_cuota'=>$this->valor_cuota,
+                'descripcion'=>$this->descripcion,
+                'sede_id'=>$this->sede_id,
+                'curso_id'=>$this->curso_id,
+                'incluye'=>false
+            ]);
+
+            foreach ($this->moduloDepen as $value) {
+                    DB::table('configpago_modulo')
+                        ->insert([
+                            'config_id'     =>$this->id,
+                            'modulo_id'     =>$value['id'],
+                            'name'          =>$value['name'],
+                            'created_at'    =>now(),
+                            'updated_at'    =>now(),
+                        ]);
+                }
+
+        }else{
+            ConfiguracionPago::whereId($this->id)->update([
+                'valor_curso'=>$this->valor_curso,
+                'valor_matricula'=>$this->valor_matricula,
+                'valor_cuota_inicial'=>$this->valor_cuota_inicial,
+                'cuotas'=>$this->cuotas,
+                'valor_cuota'=>$this->valor_cuota,
+                'descripcion'=>$this->descripcion,
+                'sede_id'=>$this->sede_id,
+                'curso_id'=>$this->curso_id,
+                'incluye'=>true
+            ]);
+        }
 
         $this->dispatch('alerta', name:'Se ha modificado correctamente la configuración de pago ');
         $this->resetFields();
--- resources/views/livewire/financiera/configuracion-pago/configuracion-pagos-crear.blade.php
+++ resources/views/livewire/financiera/configuracion-pago/configuracion-pagos-crear.blade.php
@@ -3,7 +3,7 @@
 
         <div class="grid sm:grid-cols-1 md:grid-cols-2 gap-4 m-2">
             <div class="mb-6">
-                <select wire:model.blur="sede_id" id="sede" class="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 capitalize">
+                <select wire:model.live="sede_id" id="sede" class="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 capitalize">
                     <option >Elija sede...</option>
                     @foreach ($sedes as $item)
                         <option value={{$item->id}}>{{$item->name}} - {{$item->sector->name}}</option>
