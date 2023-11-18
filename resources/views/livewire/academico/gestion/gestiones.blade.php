22a09cad05fe5c3a7a113d010fadb27952f67afa
barale2906 <alexanderbarajas@gmail.com>
2023-11-18 18:20:54 -0500

    REcibo de caja

--- resources/views/livewire/academico/gestion/gestiones.blade.php
+++ resources/views/livewire/academico/gestion/gestiones.blade.php
@@ -42,7 +42,7 @@
                         </a>
                     @endcan
                     @can('ac_estudianteCrear')
-                        <a href="" wire:click.prevent="$dispatch('created')" class="w-auto text-black bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm p-2 text-center mr-1 mb-2 capitalize" >
+                        <a href="" wire:click.prevent="$dispatch('Editando')" class="w-auto text-black bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm p-2 text-center mr-1 mb-2 capitalize" >
                             <i class="fa-solid fa-file-invoice-dollar"></i> Recibo
                         </a>
                     @endcan
@@ -252,7 +252,7 @@
     @endif
 
     @if ($is_editing)
-        <livewire:academico.matricula.matriculas-anular :elegido="$elegido" />
+        <livewire:financiera.recibo-pago.recibos-pago-crear :ruta="$ruta" />
     @endif
 
     @if ($is_deleting)
