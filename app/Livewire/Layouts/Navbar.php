f3cb66bb12f18edde6eb7e4e5570d65c7757d1a8
barale2906 <alexanderbarajas@gmail.com>
2023-12-11 10:32:38 -0500

    Indicadores diarios

--- app/Livewire/Layouts/Navbar.php
+++ app/Livewire/Layouts/Navbar.php
@@ -2,14 +2,32 @@
 
 namespace App\Livewire\Layouts;
 
+use App\Models\Academico\Control;
+use App\Models\Academico\Matricula;
+use App\Models\Configuracion\Estado;
 use App\Models\Financiera\Transaccion;
 use App\Models\Inventario\Inventario;
 use App\Models\Menu;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Navbar extends Component
 {
     protected $listeners = ['refresh' => '$refresh'];
+
+    public $sedes=[];
+
+
+    public function mount(){
+        foreach (Auth::user()->sedes as $value) {
+            if(in_array($value->id, $this->sedes )){
+
+            }else{
+                array_push($this->sedes, $value->id);
+            }
+        }
+    }
 
     private function menus(){
         return Menu::where('status',true)
@@ -28,12 +46,52 @@ class Navbar extends Component
                             ->count();
     }
 
+    private function matriculas(){
+
+        $fecha=Carbon::today();
+
+        return Matricula::where('creador_id', Auth::user()->id)
+                            ->where('created_at', '>=',$fecha)
+                            ->count();
+    }
+
+    private function proximos(){
+        return Control::where('status', true)
+                        ->whereIn('sede_id', $this->sedes)
+                        ->where('estado_cartera', 4)
+                        ->count();
+    }
+
+    private function vencidos(){
+        return Control::where('status', true)
+                        ->whereIn('sede_id', $this->sedes)
+                        ->where('estado_cartera', 5)
+                        ->count();
+    }
+
+    private function desertados(){
+
+        $estado=Estado::where('status', true)
+                        ->where('name', 'desertado')
+                        ->select('id')
+                        ->first();
+
+        return Control::where('status', true)
+                        ->whereIn('sede_id', $this->sedes)
+                        ->where('status_est', $estado->id)
+                        ->count();
+    }
+
     public function render()
     {
         return view('livewire.layouts.navbar', [
             'menus'=>$this->menus(),
             'pendInventarios'=>$this->pendInventarios(),
-            'transacciones'=>$this->transacciones()
+            'transacciones'=>$this->transacciones(),
+            'matriculas'=>$this->matriculas(),
+            'proximos'=>$this->proximos(),
+            'vencidos'=>$this->vencidos(),
+            'desertados'=>$this->desertados()
         ]);
     }
 }
--- resources/views/livewire/layouts/navbar.blade.php
+++ resources/views/livewire/layouts/navbar.blade.php
@@ -18,6 +18,22 @@
                 </div>
                 <div class="flex items-center">
 
+                    <span class="bg-red-100 text-red-800 text-sm font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-red-400 border border-red-400">
+                        <i class="fa-solid fa-triangle-exclamation fa-beat-fade"></i>Matriculas - {{$matriculas}}
+                    </span>
+
+                    <span class="bg-red-100 text-red-800 text-sm font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-red-400 border border-red-400">
+                        <i class="fa-solid fa-triangle-exclamation fa-beat-fade"></i>Vencidos - {{$vencidos}}
+                    </span>
+
+                    <span class="bg-red-100 text-red-800 text-sm font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-red-400 border border-red-400">
+                        <i class="fa-solid fa-triangle-exclamation fa-beat-fade"></i>Proximos - {{$proximos}}
+                    </span>
+
+                    <span class="bg-red-100 text-red-800 text-sm font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-red-400 border border-red-400">
+                        <i class="fa-solid fa-triangle-exclamation fa-beat-fade"></i>Desertados - {{$desertados}}
+                    </span>
+
                     @can('in_productos')
                         @if ($pendInventarios>0)
                             <a href="{{route('inventario.pend')}}" class="text-black bg-gradient-to-r from-red-300 via-red-400 to-red-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-200 dark:focus:ring-red-700 font-medium rounded-lg text-sm text-center mr-2 mb-2 capitalize">
