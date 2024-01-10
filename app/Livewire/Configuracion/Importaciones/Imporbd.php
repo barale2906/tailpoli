368acad39a2910c1ad052e086d834aa409135306
barale2906 <alexanderbarajas@gmail.com>
2024-01-10 17:09:44 -0500

    vista de carga y logica

--- app/Livewire/Configuracion/Importaciones/Imporbd.php
+++ app/Livewire/Configuracion/Importaciones/Imporbd.php
@@ -2,12 +2,61 @@
 
 namespace App\Livewire\Configuracion\Importaciones;
 
+use App\Imports\RegimenSaludImport;
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
+use Livewire\WithFileUploads;
+use Maatwebsite\Excel\Facades\Excel;
 
 class Imporbd extends Component
 {
+    use WithFileUploads;
+
+    public $archivo;
+    public $tabla;
+    public $alerta=false;
+
+    public function alarma(){
+        $this->alerta=!$this->alerta;
+    }
+
+    /**
+     * Reglas de validación
+     */
+    protected $rules = [
+        'archivo'    => 'required|mimes:xls,xlsx'
+    ];
+
+    public function importar(){
+
+        // validate
+        $this->validate();
+
+        $id=intval($this->tabla);
+
+        switch ($id) {
+            case 8:
+                Excel::import(new RegimenSaludImport, $this->archivo);
+                break;
+        }
+
+        $this->reset('archivo', 'tabla');
+        $this->dispatch('alerta', name:'Se importo correctamente el archivo ');
+
+        //refresh
+        $this->dispatch('refresh');
+
+
+    }
+
+    private function tablas(){
+        return DB::table('migrations')->get();
+
+    }
     public function render()
     {
-        return view('livewire.configuracion.importaciones.imporbd');
+        return view('livewire.configuracion.importaciones.imporbd', [
+            'tablas'=>$this->tablas()
+        ]);
     }
 }
