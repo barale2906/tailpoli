a8430ec4d6a260d5ae60478b0a830bbded10da76
barale2906 <alexanderbarajas@gmail.com>
2024-07-11 12:04:18 -0500

    Id transaccion

--- resources/views/livewire/financiera/transaccion/transacciones.blade.php
+++ resources/views/livewire/financiera/transaccion/transacciones.blade.php
@@ -120,7 +120,7 @@
                                 @else
                                     <a href="{{Storage::url($transaccione->ruta)}}" target="_blank">
                                         <button type="button" class="px-4 py-2 text-xs font-medium text-gray-900 bg-blue-150 border border-gray-200 rounded-lg hover:bg-green-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
-                                            <i class="fa-solid fa-magnifying-glass"></i>
+                                            <i class="fa-solid fa-magnifying-glass"></i> - {{$transaccione->id}}
                                         </button>
                                     </a>
                                 @endif
