06be3979ae170db1d6253c259280d2fd7c611e15
barale2906 <alexanderbarajas@gmail.com>
2024-07-10 23:35:08 -0500

    terceros 30000

--- database/seeders/FEchatransaccionSeeder.php
+++ database/seeders/FEchatransaccionSeeder.php
@@ -16,8 +16,8 @@ class FEchatransaccionSeeder extends Seeder
     {
         $recibos=Recibopago::select('created_at','numero_recibo')
                             ->whereNull('fecha_transaccion')
-                            ->where('id', '>=',30000)
-                            ->where('id', '<',60000)
+                            ->where('id', '>=',60000)
+                            ->where('id', '<',90000)
                             ->get();
 
         foreach ($recibos as $value) {
