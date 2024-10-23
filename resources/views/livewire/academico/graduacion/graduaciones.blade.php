34168a4c14a45106a0ab2402beba787cab1ff875
barale2906 <alexanderbarajas@gmail.com>
2024-10-23 15:56:40 -0500

    Ajuste filtro estudiantes desertados

--- resources/views/livewire/academico/graduacion/graduaciones.blade.php
+++ resources/views/livewire/academico/graduacion/graduaciones.blade.php
@@ -14,6 +14,9 @@
             @endif
 
         @endcan
+        <a href="#" wire:click.prevent="deserhoy" class="w-auto text-blue-800 bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 capitalize" >
+            <i class="fa-solid fa-calendar-day"></i> Desertado Hoy
+        </a>
         <div class="relative md:overflow-x-auto">
             <table class=" text-xs md:text-sm text-left text-gray-500 dark:text-gray-400">
                 <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
