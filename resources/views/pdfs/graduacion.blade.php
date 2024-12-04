d9b95cd6e729485eef1ed62ee125d3bfe8d4483d
barale2906 <alexanderbarajas@gmail.com>
2024-12-04 12:55:39 -0500

    Ajuste subtitulo

--- resources/views/pdfs/graduacion.blade.php
+++ resources/views/pdfs/graduacion.blade.php
@@ -89,7 +89,6 @@
             }
 
             .font-subtitulo{
-                font-style: italic;
                 color: #A8518A;
             }
 
@@ -200,7 +199,7 @@
                     @break
 
                 @case('subtitulo')
-                    <h1 class="centrado uppercase font-xl font-subtitulo bold font-parrafo">
+                    <h1 class="centrado uppercase font-xl font-subtitulo bold font-cursiva">
                         {{$item['contenido']}}
                     </h1>
                     @break
