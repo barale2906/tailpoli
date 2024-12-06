7d49bd7e02728e2b56d1acf6991251a602c46a94
barale2906 <alexanderbarajas@gmail.com>
2024-12-06 12:23:31 -0500

    Número de recibo en el cierre

--- resources/views/includes/cierrerecibos.blade.php
+++ resources/views/includes/cierrerecibos.blade.php
@@ -31,7 +31,7 @@
         @foreach ($recibos as $recibo)
             <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-green-200 text-sm">
                 <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    {{$recibo->id}}
+                    {{$recibo->numero_recibo}}
                 </th>
                 <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white capitalize">
                     {{$recibo->fecha}}
