559b03f8101e9eaa2f3da2efd56765ac22a758d2
barale2906 <alexanderbarajas@gmail.com>
2024-12-13 22:32:54 -0500

    Ajuste Medio

--- resources/views/livewire/academico/matricula/matriculas-crear.blade.php
+++ resources/views/livewire/academico/matricula/matriculas-crear.blade.php
@@ -289,10 +289,17 @@
                             <select wire:model.blur="medio" id="medio" class="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                 <option >Elija su respuesta...</option>
                                 <option value="Google">Google</option>
+                                <option value="Facebook">Facebook</option>
+                                <option value="Instagram">Instagram</option>
+                                <option value="Tik Tok">Tik Tok</option>
+                                <option value="X">X</option>
                                 <option value="Página Web">Página Web</option>
+                                <option value="Recomendación">Recomendación</option>
                                 <option value="Redes Sociales">Redes Sociales</option>
                                 <option value="Referencia">Referencia</option>
                                 <option value="Volante">Volante</option>
+                                <option value="Correo Electrónico">Correo Electrónico</option>
+                                <option value="Visita en sitio">Visita en sitio</option>
                                 <option value="Otro">Otro</option>
                             </select>
                             @error('medio')
