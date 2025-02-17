52f7d6d5bfa80ead0c059da180c63998ad86812f
barale2906 <alexanderbarajas@gmail.com>
2025-02-16 20:10:29 -0500

    Descuento inventario

--- resources/views/livewire/inventario/inventario/salida.blade.php
+++ resources/views/livewire/inventario/inventario/salida.blade.php
@@ -182,7 +182,7 @@
                             </div>
                         @endif
 
-                        <div class="mb-6">
+                        {{-- <div class="mb-6">
                             <label for="descuento" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Descuento</label>
                             <input type="text" id="descuento" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="descuento" wire:model.live="descuento">
                             @error('descuento')
@@ -190,7 +190,7 @@
                                     <span class="font-medium">¡IMPORTANTE!</span>  {{ $message }} .
                                 </div>
                             @enderror
-                        </div>
+                        </div> --}}
 
                         <div>
                             @if ($cantidad>0 && $producto)
