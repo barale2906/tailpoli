293e4bc709933c0c584cdaf306f12876213d133c
barale2906 <alexanderbarajas@gmail.com>
2025-02-23 18:15:27 -0500

    Ajuste descuento

--- app/Livewire/Financiera/Descuento/Descugestiona.php
+++ app/Livewire/Financiera/Descuento/Descugestiona.php
@@ -2,9 +2,13 @@
 
 namespace App\Livewire\Financiera\Descuento;
 
+use App\Models\Financiera\ConceptoPago;
 use App\Models\Financiera\Descuento;
 use App\Traits\CrtStatusTrait;
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
+
+use function Laravel\Prompts\table;
 
 class Descugestiona extends Component
 {
@@ -15,6 +19,10 @@ class Descugestiona extends Component
     public $tipo;
     public $aplica;
     public $actual;
+    public $conceptos;
+    public $tiponom;
+    public $asignados;
+
 
     public function mount($elegido=null){
         $this->resetFields();
@@ -29,6 +37,33 @@ class Descugestiona extends Component
         $this->valor = $this->actual->valor;
         $this->tipo = $this->actual->tipo;
         $this->aplica = $this->actual->aplica;
+
+        $this->concep();
+    }
+
+    public function concep(){
+
+        $this->reset('tiponom');
+
+        switch (intval($this->aplica)) {
+            case 0:
+                $this->tiponom="cartera";
+                break;
+
+            case 1:
+                $this->tiponom="inventario";
+                break;
+
+            case 2:
+                $this->tiponom="otro";
+                break;
+        }
+        $this->conceptos=ConceptoPago::where('tipo', $this->tiponom)
+                                        ->where('status', 1)
+                                        ->orderBy('name', 'ASC')
+                                        ->get();
+
+        $this->concepasignados();
     }
 
     /**
@@ -70,22 +105,23 @@ class Descugestiona extends Component
                         'status' => 0
                     ]);
 
-        Descuento::create([
-                        'name' => strtolower($this->name),
-                        'valor' => $this->valor,
-                        'tipo' => $this->tipo,
-                        'aplica' => $this->aplica,
-                        'status' => 1
-                    ]);
+        $this->actual=Descuento::create([
+                                    'name' => strtolower($this->name),
+                                    'valor' => $this->valor,
+                                    'tipo' => $this->tipo,
+                                    'aplica' => $this->aplica,
+                                    'status' => 1
+                                ]);
 
 
 
         // Notificación
         $this->dispatch('alerta', name:'Se ha creado correctamente el descuento: '.$this->name);
-        $this->resetFields();
+        //$this->resetFields();
 
         //refresh
-        $this->dispatch('volviendo');
+        //$this->dispatch('volviendo');
+        $this->concep();
     }
 
     public function editar(){
@@ -100,12 +136,51 @@ class Descugestiona extends Component
                         'aplica' => $this->aplica,
                     ]);
 
+        if(intval($this->aplica) !== $this->actual->aplica){
+            DB::table('descuento_producto')
+                ->where('descuento_id', $this->actual->id)
+                ->delete();
+
+        }
+
         // Notificación
         $this->dispatch('alerta', name:'Se ha modificado correctamente el descuento: '.$this->name);
-        $this->resetFields();
+        //$this->resetFields();
 
         //refresh
-        $this->dispatch('volviendo');
+        //$this->dispatch('volviendo');
+        $this->concep();
+
+    }
+
+    public function cargar($id){
+        DB::table('descuento_producto')
+            ->insert([
+                'descuento_id'      => $this->actual->id,
+                'concepto_pago_id'  => $id,
+                'created_at'        => now(),
+                'updated_at'        => now()
+            ]);
+
+        $this->concepasignados();
+    }
+
+    public function eliminar($id){
+        DB::table('descuento_producto')
+            ->where('id', $id)
+            ->delete();
+
+        $this->concepasignados();
+    }
+
+    public function concepasignados(){
+
+        $this->asignados=DB::table('concepto_pagos')
+                            ->join('descuento_producto', 'concepto_pagos.id', '=', 'descuento_producto.concepto_pago_id')
+                            ->where('descuento_producto.descuento_id',$this->actual->id)
+                            ->select('concepto_pagos.*','descuento_producto.id as elegido')
+                            ->orderBy('concepto_pagos.name', 'ASC')
+                            ->get();
     }
 
     public function render()
--- resources/views/livewire/financiera/descuento/descugestiona.blade.php
+++ resources/views/livewire/financiera/descuento/descugestiona.blade.php
@@ -6,7 +6,7 @@
             crear descuento
         @endif
     </h1>
-    <div class="grid sm:grid-cols-1 md:grid-cols-4 gap-4 m-3">
+    <div class="grid sm:grid-cols-1 md:grid-cols-5 gap-4 m-3">
         <div></div>
         <div>
             <div class="mb-6">
@@ -64,11 +64,47 @@
                     <i class="fa-solid fa-tags"></i> Crear Descuento
                 </a>
             @endif
+            <br>
             <a href="#" wire:click.prevent="regresar()" class="text-black bg-gradient-to-r from-red-300 via-red-400 to-red-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-200 dark:focus:ring-red-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 capitalize">
                 <i class="fa-solid fa-rectangle-xmark"></i> cancelar
             </a>
         </div>
-        <div></div>
+        @if ($actual)
+            <div class="block max-w-sm p-6 bg-white border border-gray-200 ring rounded-2xl shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                <a href="" wire:click.prevent="" >
+
+                    <h5 class="mb-2 text-sm md:text-xl font-bold tracking-tight text-gray-900 dark:text-white uppercase">
+                        conceptos de pago
+                    </h5>
+                    @foreach ($conceptos as $item)
+                        <p class="font-normal text-xs md:text-lg text-justify text-gray-700 dark:text-gray-400 capitalize">
+                            <a href="" wire:click.prevent="cargar({{$item->id}})">
+                                {{$item->name}} - <i class="fa-solid fa-check-double text-green-500 text-xs"></i>
+                            </a>
+                        </p>
+                    @endforeach
+                </a>
+            </div>
+            @if ($asignados)
+                <div class="block max-w-sm p-6 bg-white border border-gray-200 ring rounded-2xl shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                    <a href="" wire:click.prevent="" >
+
+                        <h5 class="mb-2 text-sm md:text-xl font-bold tracking-tight text-gray-900 dark:text-white uppercase">
+                            Asignados
+                        </h5>
+                        @foreach ($asignados as $item)
+                            <p class="font-normal text-xs md:text-lg text-justify text-gray-700 dark:text-gray-400 capitalize">
+                                <a href="" wire:click.prevent="eliminar({{$item->elegido}})">
+                                    {{$item->name}} - {{$item->elegido}} - <i class="fa-solid fa-trash-can text-red-600 text-xs"></i>
+                                </a>
+                            </p>
+                        @endforeach
+                    </a>
+                </div>
+            @endif
+
+        @endif
+
         <div></div>
     </div>
 </div>
