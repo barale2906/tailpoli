f97c0fee44a73376ba14587a6df1f6576e8fa4f2
barale2906 <alexanderbarajas@gmail.com>
2025-05-09 16:43:54 -0500

    Cancelación gestión de cobranza

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,13 +24,13 @@ class Kernel extends ConsoleKernel
         $schedule->command('Matricula:bienvenida-email')->timezone('America/Bogota')->at('02:45');  //everyMinute();
         $schedule->command('Cartera:aviso-cartera')->timezone('America/Bogota')->at('02:50'); //->at('02:50');  //everyMinute();
         $schedule->command('Academico:Gastos')->timezone('America/Bogota')->at('03:10'); //->at('03:10');  //everyMinute();
-        $schedule->command('Cobranza:Carga')->timezone('America/Bogota')->at('03:40'); //->at('03:40');  //everyMinute();
+        //$schedule->command('Cobranza:Carga')->timezone('America/Bogota')->at('03:40'); //->at('03:40');  //everyMinute();
         $schedule->command('Cartera:Castiga')->timezone('America/Bogota')->at('04:00'); //->at('04:00');  //everyMinute();
-        $schedule->command('Cobranza:Descarga')->timezone('America/Bogota')->at('04:40'); //->at('04:10');  //everyMinute();
+        //$schedule->command('Cobranza:Descarga')->timezone('America/Bogota')->at('04:40'); //->at('04:10');  //everyMinute();
         $schedule->command('Academico:Graduado')->timezone('America/Bogota')->at('05:40');  //->everyThreeMinutes();
         //$schedule->command('Cartera:DescargaMora')->timezone('America/Bogota')->at('05:50');  //->everyThreeMinutes();
         $schedule->command('Cartera:Reset')->timezone('America/Bogota')->at('05:50');  //->everyThreeMinutes();
-        $schedule->command('Cobranza:Gestion')->timezone('America/Bogota')->at('10:45'); //->at('10:45');  //everyMinute();
+        //$schedule->command('Cobranza:Gestion')->timezone('America/Bogota')->at('10:45'); //->at('10:45');  //everyMinute();
 
     }
     /**
